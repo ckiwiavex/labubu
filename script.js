7fad57240f0e2bf25dd222bfa0ff4696e274f9d7
ckiwiavex <kevinhzhz672@gmail.com>
2026-07-04 19:25:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,21 @@ const sobre = document.querySelector(".sobre");
 const tapa = document.querySelector(".tapa");
 const carta = document.querySelector(".carta");
 
+const inicio = document.getElementById("inicio");
+const bienvenida = document.getElementById("bienvenida");
+
 sobre.addEventListener("click",()=>{
 
 tapa.style.transform="rotateX(180deg)";
 
 carta.style.transform="translateY(-45px)";
+
+setTimeout(()=>{
+
+inicio.style.display="none";
+
+bienvenida.style.display="flex";
+
+},1200);
 
 });
