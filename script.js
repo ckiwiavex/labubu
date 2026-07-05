bcc5060da2997edd1e72d7a58990092a7bb04bd6
ckiwiavex <kevinhzhz672@gmail.com>
2026-07-04 19:21:01 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const sobre = document.querySelector(".sobre");
+const tapa = document.querySelector(".tapa");
+const carta = document.querySelector(".carta");
 
 sobre.addEventListener("click",()=>{
 
-alert("¡Bienvenida! ❤️");
+tapa.style.transform="rotateX(180deg)";
+
+carta.style.transform="translateY(-45px)";
 
 });
