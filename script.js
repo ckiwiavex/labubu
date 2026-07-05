468e842ec63c6cf41a45af773430df2348895e2b
ckiwiavex <kevinhzhz672@gmail.com>
2026-07-04 17:29:40 -0700

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,78 @@ boton.addEventListener("click",()=>{
     });
 
 });
+#galeria{
+
+padding:100px 8%;
+
+}
+
+#galeria h2{
+
+text-align:center;
+
+margin-bottom:50px;
+
+font-size:45px;
+
+color:#ff70ad;
+
+}
+
+.fotos{
+
+display:grid;
+
+grid-template-columns:repeat(auto-fit,minmax(230px,1fr));
+
+gap:35px;
+
+}
+
+.foto{
+
+background:white;
+
+padding:15px;
+
+border-radius:18px;
+
+box-shadow:0 15px 30px rgba(0,0,0,.1);
+
+transition:.4s;
+
+transform:rotate(-2deg);
+
+}
+
+.foto:nth-child(even){
+
+transform:rotate(2deg);
+
+}
+
+.foto:hover{
+
+transform:scale(1.05) rotate(0deg);
+
+}
+
+.foto img{
+
+width:100%;
+
+border-radius:12px;
+
+display:block;
+
+}
+
+.foto p{
+
+margin-top:15px;
+
+text-align:center;
+
+font-size:17px;
+
+}
